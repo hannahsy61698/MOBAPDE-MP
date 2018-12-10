a2770b27907b3ff7b5413459e78e5ee242d02943
Angel <angelica_limkico@dlsu.edu.ph>
2018-12-10 19:49:40 +0800

    changed to portrait

--- app/src/main/java/com/example/user/mp/MainActivity.java
+++ app/src/main/java/com/example/user/mp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.user.mp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button butt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity);
+
+        butt = findViewById(R.id.butts);
+
+
     }
 }
