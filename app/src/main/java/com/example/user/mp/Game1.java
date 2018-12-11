a5149d664031d0c7398ba6539ce6fb24517ce2b7
Angel <angelica_limkico@dlsu.edu.ph>
2018-12-11 13:54:43 +0800

    goes tot the next activity

---
+++ app/src/main/java/com/example/user/mp/Game1.java
@@ -0,0 +1,13 @@
+package com.example.user.mp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Game1 extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game1);
+
+    }
+}
--- app/src/main/java/com/example/user/mp/MainActivity.java
+++ app/src/main/java/com/example/user/mp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.mp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +13,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity);
-
-        butt = findViewById(R.id.butts);
+        setContentView(R.layout.activity_main);
 
 
+        Button next = (Button) findViewById(R.id.butts);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Game1.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
