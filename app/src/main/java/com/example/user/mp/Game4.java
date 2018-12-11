f22943f6b847ae5c461b4813bdd7ed29f8e95465
Hannah Sy <hannah_idette_sy@dlsu.edu.ph>
2018-12-12 06:19:55 +0800

    lvl 4 working
    skipped lvl3

---
+++ app/src/main/java/com/example/user/mp/Game4.java
@@ -0,0 +1,57 @@
+package com.example.user.mp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Game4 extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game4);
+
+        ImageView img3 = new ImageView(this);
+        img3 = findViewById(R.id.correct_btn);
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Game3.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+    public void next(View view){
+        Intent myIntent = new Intent(view.getContext(), Game5.class);
+        startActivityForResult(myIntent, 0);
+    }
+
+    //copy start here to exit
+    boolean doubleBackToExitPressedOnce = false;
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("Exit me", true);
+            startActivity(intent);
+            finish();
+
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+//end here
+
+}
