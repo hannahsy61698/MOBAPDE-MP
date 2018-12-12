3909a0ae58c74b9025b352ad98043df9b64cad63
Hannah Sy <hannah_idette_sy@dlsu.edu.ph>
2018-12-12 23:49:12 +0800

    game 3 works

--- app/src/main/java/com/example/user/mp/Game3_0.java
+++ app/src/main/java/com/example/user/mp/Game3_0.java
@@ -1,14 +1,54 @@
 package com.example.user.mp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Game3_0 extends Activity {
 
+    private Button img;
+    private Button next;
+
+    private EditText text;
+
+    private String lives;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game3_0);
+
+        img = findViewById(R.id.clickmebtn);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Game3.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+
+        next = findViewById(R.id.submitbtn);
+        text = findViewById(R.id.character);
+
+        final String txt = text.getText().toString();
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txt.equals("stitch") || txt.equals("STITCH") || txt.equals("Stitch")) {
+                    Intent myIntent = new Intent(view.getContext(), Game4.class);
+                    myIntent.putExtra("lives", lives);
+                    startActivityForResult(myIntent, 0);
+                }
+            }
+        });
+
+
+
 
     }
 
