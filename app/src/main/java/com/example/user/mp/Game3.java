ade75835fc2c28b2bee9bd5f6be02345a6ae8c88
Hannah Sy <hannah_idette_sy@dlsu.edu.ph>
2018-12-12 15:28:56 +0800

    ???

---
+++ app/src/main/java/com/example/user/mp/Game3.java
@@ -0,0 +1,24 @@
+package com.example.user.mp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Game3 extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game3);
+
+        Button temp = new Button(this);
+        temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Game4.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+}
