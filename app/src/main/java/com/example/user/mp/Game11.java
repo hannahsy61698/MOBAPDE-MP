e6ad79f956ac1be352f4416d4e3757bd7e8cf0d2
Hannah Sy <hannah_idette_sy@dlsu.edu.ph>
2018-12-12 23:32:09 +0800

    idk

--- app/src/main/java/com/example/user/mp/Game11.java
+++ app/src/main/java/com/example/user/mp/Game11.java
@@ -1,4 +1,41 @@
 package com.example.user.mp;
 
-public class Game11 {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class Game11 extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game11);
+
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("Exit me", true);
+            startActivity(intent);
+            finish();
+
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+//end here
 }
--- app/src/main/java/com/example/user/mp/Game9.java
+++ app/src/main/java/com/example/user/mp/Game9.java
@@ -3,7 +3,9 @@ package com.example.user.mp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 public class Game9 extends Activity {
 
@@ -18,4 +20,29 @@ public class Game9 extends Activity {
         Intent myIntent = new Intent(view.getContext(), Game10.class);
         startActivityForResult(myIntent, 0);
     }
+
+    boolean doubleBackToExitPressedOnce = false;
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.putExtra("Exit me", true);
+            startActivity(intent);
+            finish();
+
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+//end here
 }
